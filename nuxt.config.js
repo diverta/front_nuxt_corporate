d19397dbbc0622beec3240ebc0a00b92e231143f
VortexExpansion <gaurav@diverta.co.jp>
2023-09-18 14:11:44 +0530

    add meta, rmv button

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,25 @@
 export default {
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL
-    }
+      baseURL: process.env.BASE_URL,
+    },
   },
+  app: {
+    head: {
+      title: "front_nuxt_corporate",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+
   devtools: { enabled: true },
-  css: [
-    '@/assets/scss/style.scss'
-  ],
-}
+  css: ["@/assets/scss/style.scss"],
+};
