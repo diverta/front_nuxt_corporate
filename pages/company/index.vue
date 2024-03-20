f587fc2f436791236004f2edcdb76ed5da0620f2
VortexExpansion <gaurav@diverta.co.jp>
2024-03-20 15:48:30 +0900

    upd company to csr

--- pages/company/index.vue
+++ pages/company/index.vue
@@ -1,19 +1,23 @@
 <template>
-  <div>
-    <UiPageHeader :subject="response.details.subject" subheading="Company" />
-
-    <article class="c-article">
-      <div class="l-container--large l-container--contents">
-        <div v-html="response.details.contents"></div>
-      </div>
-    </article>
-  </div>
+  <ClientOnly>
+    <div>
+      <UiPageHeader :subject="response.details.subject" subheading="Company" />
+      <article class="c-article">
+        <div class="l-container--large l-container--contents">
+          <div v-html="response.details.contents"></div>
+        </div>
+      </article>
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 
-const { data: response } = await useFetch(
-  `${config.public.kurocoApiDomain}/rcms-api/1/content/details/company`
+const response = await $fetch(
+  `${config.public.kurocoApiDomain}/rcms-api/1/content/details/company`,
+  {
+    server: false,
+  }
 );
 </script>
