7a1558f0a2b82c396492327b78cbe17f51e980d1
VortexExpansion <gaurav@diverta.co.jp>
2024-03-20 16:50:48 +0900

    privacy page

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>
-    <UiPageHeader
-      :subject="response.details.subject"
-      subheading="Privacy Policy"
-    />
-
-    <article class="c-article">
-      <div class="l-container--large l-container--contents">
-        <div v-html="response.details.contents"></div>
-      </div>
-    </article>
-  </div>
+  <ClientOnly>
+    <div>
+      <UiPageHeader
+        :subject="response.details.subject"
+        subheading="Privacy Policy"
+      />
+      <article class="c-article">
+        <div class="l-container--large l-container--contents">
+          <div v-html="response.details.contents"></div>
+        </div>
+      </article>
+    </div>
+  </ClientOnly>
 </template>
 
 <script setup>
 const config = useRuntimeConfig();
 
-const { data: response } = await useFetch(
+const response = await $fetch(
   `${config.public.kurocoApiDomain}/rcms-api/1/content/details/privacy`,
   {
     credentials: 'include',
+    server: false
   }
 );
 </script>
