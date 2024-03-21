ae5e1e3e550997cb49cf8280c0b7ee29c2a37187
VortexExpansion <gaurav@diverta.co.jp>
2024-03-21 16:58:30 +0900

    rmv ClientOnly chore

--- pages/mypage/edit/index.vue
+++ pages/mypage/edit/index.vue
@@ -1,69 +1,57 @@
 <template>
-  <ClientOnly>
-    <div>
-      <UiPageHeader
-        :path="[{ label: 'マイページ', to: '/mypage/' }]"
-        subject="マイページ"
-        subheading="Edit Profile"
-      />
+  <div>
+    <UiPageHeader
+      :path="[{ label: 'マイページ', to: '/mypage/' }]"
+      subject="マイページ"
+      subheading="Edit Profile"
+    />
 
-      <section>
-        <div class="l-container--small l-container--contents">
-          <template v-if="updateProfileDone">
-            <UiAlertSuccess :message="message" />
-            <div class="c-form u-text-align-center">
-              <NuxtLink to="/mypage">マイページへ戻る</NuxtLink>
+    <section>
+      <div class="l-container--small l-container--contents">
+        <template v-if="updateProfileDone">
+          <UiAlertSuccess :message="message" />
+          <div class="c-form u-text-align-center">
+            <NuxtLink to="/mypage">マイページへ戻る</NuxtLink>
+          </div>
+        </template>
+        <template v-else>
+          <UiAlertError v-if="error" :error="error" />
+          <form @submit.prevent="handleUpdateProfile" class="c-form">
+            <div class="c-form-group">
+              <label for="name1" class="c-form-label">名前（姓）</label>
+              <span class="c-form-label__required">*</span>
+              <input v-model="user.name1" name="name1" id="name1" type="text" />
             </div>
-          </template>
-          <template v-else>
-            <UiAlertError v-if="error" :error="error" />
-            <form @submit.prevent="handleUpdateProfile" class="c-form">
-              <div class="c-form-group">
-                <label for="name1" class="c-form-label">名前（姓）</label>
-                <span class="c-form-label__required">*</span>
-                <input
-                  v-model="user.name1"
-                  name="name1"
-                  id="name1"
-                  type="text"
-                />
-              </div>
-              <div class="c-form-group">
-                <label for="name2" class="c-form-label">名前（名）</label>
-                <input
-                  v-model="user.name2"
-                  name="name2"
-                  id="name2"
-                  type="text"
-                />
-              </div>
-              <div class="c-form-group">
-                <label for="email" class="c-form-label">メールアドレス</label>
-                <input
-                  v-model="user.email"
-                  name="email"
-                  id="email"
-                  type="email"
-                />
-              </div>
-              <div class="c-form-group">
-                <label class="c-form-label">会員種別</label>
-                <div>{{ statusText }}</div>
-              </div>
-              <div class="c-form-group">
-                <UiSubmitButton type="submit" :loading="loading">
-                  更新する
-                </UiSubmitButton>
-              </div>
-              <div class="c-form-group u-text-align-center">
-                <NuxtLink to="/mypage/">マイページへ戻る</NuxtLink>
-              </div>
-            </form>
-          </template>
-        </div>
-      </section>
-    </div>
-  </ClientOnly>
+            <div class="c-form-group">
+              <label for="name2" class="c-form-label">名前（名）</label>
+              <input v-model="user.name2" name="name2" id="name2" type="text" />
+            </div>
+            <div class="c-form-group">
+              <label for="email" class="c-form-label">メールアドレス</label>
+              <input
+                v-model="user.email"
+                name="email"
+                id="email"
+                type="email"
+              />
+            </div>
+            <div class="c-form-group">
+              <label class="c-form-label">会員種別</label>
+              <div>{{ statusText }}</div>
+            </div>
+            <div class="c-form-group">
+              <UiSubmitButton type="submit" :loading="loading">
+                更新する
+              </UiSubmitButton>
+            </div>
+            <div class="c-form-group u-text-align-center">
+              <NuxtLink to="/mypage/">マイページへ戻る</NuxtLink>
+            </div>
+          </form>
+        </template>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
@@ -71,7 +59,7 @@ const config = useRuntimeConfig();
 
 const { authUser, profile } = useAuth();
 
-const message = '会員情報の更新が完了しました。';
+const message = "会員情報の更新が完了しました。";
 const updateProfileDone = ref(false);
 const error = ref(null);
 const loading = ref(false);
@@ -84,22 +72,22 @@ const user = ref({
 
 const statusText = computed(() => {
   if (authUser.value.isPremiumUser) {
-    return 'プレミアム会員';
+    return "プレミアム会員";
   }
   if (authUser.value.isRegularUser) {
-    return '通常会員';
+    return "通常会員";
   }
-  return '';
+  return "";
 });
 
 const handleUpdateProfile = async () => {
   loading.value = true;
   try {
     await $fetch(`${config.public.kurocoApiDomain}/rcms-api/1/member/update`, {
-      credentials: 'include',
-      method: 'POST',
+      credentials: "include",
+      method: "POST",
       body: { ...user.value },
-      server: false
+      server: false,
     });
     await profile();
     updateProfileDone.value = true;
